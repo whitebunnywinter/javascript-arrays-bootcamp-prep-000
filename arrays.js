934ea7eb56a8b9d8f10e652f89ad677a9c0d5bce
Haruka Matsumoto <matsumoto.haru@gmail.com>
2017-12-20 06:26:50 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,25 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  var b = [...array]
+  b.unshift(element)
+  return b
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  var b = [...array]
+  b.push(element)
+  return b
+}
+
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
